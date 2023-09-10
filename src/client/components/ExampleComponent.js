f3faeb030dbb09c921d5f25b5b3e2e2b6ad530e5
Parcival <ipavlenko239@gmail.com>
2023-09-10 13:05:35 +0300

    changed component

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1>Welcome to My Project</h1>;
+	return <>
+	<h1>Welcome to My Project</h1>
+	<p>My name is Parcival</p>
+	<p>I choose that name because I liked it but in reality my name is Ihor Pavlenko</p></>
+
 }
